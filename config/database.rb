25a2413d67d853dcb6b415f2f17b4731c90636f3
jeferbc <jeffe.bernal@gmail.com>
2020-03-23 16:54:15 -0500

    production

--- config/database.rb
+++ config/database.rb
@@ -2,6 +2,11 @@ require 'sequel'
 
 Sequel::Model.plugin :json_serializer
 
-DB = Sequel.connect(adapter: :postgres, database: "theaters_#{ENV['RACK_ENV']}",
-                                        host: 'localhost',
-                                        username: 'postgres', password: '')
+
+if ENV['RACK_ENV'] == 'production'
+  DB = Sequel.connect(adapter: :postgres, url: ENV['DATABASE_URL'])
+else
+  DB = Sequel.connect(adapter: :postgres, database: "theaters_#{ENV['RACK_ENV']}",
+                                          host: 'localhost',
+                                          username: 'postgres', password: '')
+end
